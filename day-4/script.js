db195a32c8cb8a1898e6b211e480ac8cf4aa3810
charleselm <charleselmore53@icloud.com>
2025-03-12 17:50:35 -0400

    Update day-4.

--- day-4/script.js
+++ day-4/script.js
@@ -24,10 +24,12 @@ function partOneA () {
 function partOneB () {
   alert("Try it!");
   // 1. Create a for loop.
-
+  let highNumber = 7;
+  for (let i = 0; i < highNumber; i++) {
     // 2. give the user and alert with the current number.
-
-}
+    alert(`The number is ${i}`);
+  }
+} 
 
 // Example
 // A for loop over an array
@@ -54,13 +56,14 @@ function partTwoB () {
   messageParagraph.innerHTML = "Flavors";
 
   // 1. Create an array of 5 flavors
-
+  let flavors = ["Vanilla", "Strawberry", "Chocolate", "Cherry", "Raspberry"];
   // 2. Find the length of the array
-
+  let highNumber = flavors.length;
   // 3. Create a for loop.
-
+  for (let i = 0; i < highNumber; i++) {
     // 4. give the user and alert with the current friend.
-
+    alert(`The flavor is ${flavors[i]}`);
+  }
 }
 
 // Example
@@ -86,12 +89,14 @@ function partThreeB () {
 
   alert("Try it!");
   // 1. create an array of 5 flavors
-
+  let flavors = ["Vanilla", "Strawberry", "Chocolate", "Cherry", "Raspberry"];
   // 2. find the length of the array
- 
+  let highNumber = flavors.length;
   // 3. create a for loop
-
+  for (let i=0; i < highNumber; i++) {
     // 4. write the friend to the innerHTML of the list
+    messageList.innerHTML += `<li>${flavors[i]}</li>`;
+  }
 }
 
 // Example
@@ -130,9 +135,9 @@ function partFourB () {
   let newFlavor, highNumber;
 
   // 1. prompt for a flavor
-
+  newFlavor = prompt("Add a new flavor to the list.");
   // 2. add the flavor to the array with push()
-
+  flavors.push(newFlavor);
   // 3. reset the list in the innerHTML
   messageList.innerHTML = "";
   // 4. set the message to "Flavors"
@@ -142,7 +147,9 @@ function partFourB () {
   highNumber = flavors.length;
 
   // 6. write all the flavors back to the HTML list from the array, updated with our new flavor.
-
+  for (let i = 0; i < highNumber; i++) {
+    messageList.innerHTML += `<li>${flavors[i]}</li>`;
+  }
 }
 
 // BONUS
