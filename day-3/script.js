41e2943074d71113f44be0ef680f085d94c48eaf
charleselm <charleselmore53@icloud.com>
2025-03-12 17:35:06 -0400

    Update day-3.

--- day-3/script.js
+++ day-3/script.js
@@ -26,9 +26,13 @@ function partOneB () {
   let friends 
   friends = [];
   // 1. TODO: Add your own friends to the array
-
+  friends[0] = "Toni";
+  friends[1] = "Bettina";
+  friends[2] = "Ginny";
+  friends[3] = "Maddy";
+  friends[4] = "Kirsten";
   // 2. TODO: write your friends to the message
-
+  messageParagraph.innerHTML = `Your friends are ${friends[0]}, ${friends[1]}, ${friends[2]}, ${friends[3]}, and ${friends[4]}.`
 }
 
 // Example
@@ -56,9 +60,18 @@ function partTwoA () {
 function partTwoB () {
   let friends = [];
   // 1. Add your friends to the array
-
+  friends[0] = "Toni";
+  friends[1] = "Bettina";
+  friends[2] = "Ginny";
+  friends[3] = "Maddy";
+  friends[4] = "Kirsten";
   // 2. write your friends to the innerHTML of the document
-
+  messageParagraph.innerHTML = "Your friends are ";
+  messageParagraph.innerHTML += `${friends[0]}`;
+  messageParagraph.innerHTML += `, ${friends[1]}`;
+  messageParagraph.innerHTML += `, ${friends[2]}`;
+  messageParagraph.innerHTML += `, ${friends[3]}`;
+  messageParagraph.innerHTML += `, ${friends[4]}`;
 }
 
 // Example
@@ -75,8 +88,9 @@ function partThreeA () {
 // Try prompting for a friend with a slightly different prompt or writing the message in a slightly different format.
 function partThreeB () {
   // 1. prompt for a friend
-
+  let friend = prompt("Enter a friend's name.");
   // 2. add the friend to the message
+  messageParagraph.innerHTML += `, ${friend}`;
 }
 
 // Example
@@ -109,13 +123,13 @@ function partFourB () {
   totalFavFlavors = myFavFlavors.length;
 
   // 1. prompt for a flavor
-
+  newFavFlavor = prompt(`You have ${totalFavFlavors} favorite flavors. To add another, enter a new favorite flavor.`);
   // 2. add the flavor to the array with push()
-
+  myFavFlavors.push(newFavFlavor);
   // 3. add the flavor to the message
-
+  messageList.innerHTML += `<li>${newFavFlavor}</li>`;
   // 4. log the array to the console for debugging
-
+  console.log(myFavFlavor);
 }
 
 // BONUS: Try to add your own button to the page that will accept user input to add to the page.
